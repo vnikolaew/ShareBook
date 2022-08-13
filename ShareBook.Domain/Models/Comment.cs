506ef9e4d823b1f71c356215589e3ed3a96f5567
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:29:12 +0300

    Added a Comment Domain entity ...

---
+++ ShareBook.Domain/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using ShareBook.Domain.Common;
+using ShareBook.Domain.Models.Post;
+
+namespace ShareBook.Domain.Models;
+public class Comment : AuditableEntity<Guid>
+{
+  public Content Content { get; private set; }
+
+  public User.User Author { get; private set; }
+
+  public Post.Post Post { get; private set; }
+
+  public Comment(User.User author, Content content, Post.Post post)
+  {
+    Author = author;
+    Content = content;
+    Post = post;
+  }
+
+  public Comment() { }
+
+  public void EditContent(Content content) => Content = content;
+}
