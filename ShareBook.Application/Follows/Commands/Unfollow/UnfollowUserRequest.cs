99bbbfbf8203aeb27a1a8b2f6bf36eb4ccf7804f
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:48:58 +0300

    Added a definition for a Follow Service ...

---
+++ ShareBook.Application/Follows/Commands/Unfollow/UnfollowUserRequest.cs
@@ -0,0 +1,29 @@
+﻿using ShareBook.Application.Common;
+using ShareBook.Application.Common.Contracts;
+using ShareBook.Shared.Abstractions.Requests;
+
+namespace ShareBook.Application.Follows.Commands.Follow;
+public class UnfollowUserRequest : EntityCommand<Guid>, IRequest<Result>
+{
+	public class UnfollowUserHandler : IRequestHandler<UnfollowUserRequest, Result>
+	{
+		private readonly IFollowService _follows;
+		private readonly ICurrentUser _currentUser;
+
+		public UnfollowUserHandler(
+			IFollowService follows,
+			ICurrentUser currentUser)
+		{
+			_follows = follows;
+			_currentUser = currentUser;
+		}
+
+		public async Task<Result> Handle(UnfollowUserRequest request, CancellationToken cancellationToken = default)
+		{
+			var result = await _follows.Unfollow(_currentUser.UserId, request.Id);
+			return result
+				? Result.Success
+				: Result.Failure(new[] {"You are not following the requested user. "});
+		}
+	}
+}
---
+++ ShareBook.Application/Follows/IFollowService.cs
@@ -0,0 +1,11 @@
+﻿using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Application.Follows;
+
+public interface IFollowService
+{
+	Task<bool> Follow(UserId followerId, UserId followeeId, DateTime dateTime);
+	Task<bool> Unfollow(UserId followerId, UserId followeeId);
+	Task<IEnumerable<Domain.Relationships.Follows>> GetFollowers(UserId followeeId);
+	Task<IEnumerable<ShareBook.Domain.Relationships.Follows>> GetFollowing(UserId followerId);
+}
