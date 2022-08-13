bb9c2d830f1503c5971590893fb7b8fe14e74b39
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:51:47 +0300

    Added a feature for editing comments ...

---
+++ ShareBook.Application/Comments/Commands/Edit/CreateCommentValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ShareBook.Application.Comments.Commands.Edit;
+using static ShareBook.Domain.Models.ModelConstants.Post;
+
+namespace ShareBook.Application.Comments.Commands.Create;
+
+public class EditCommentValidator : AbstractValidator<EditCommentRequest>
+{
+	public EditCommentValidator()
+		=> RuleFor(r => r.Content)
+		   .MinimumLength(MinContentLength)
+		   .MaximumLength(MaxContentLength);
+}
---
+++ ShareBook.Application/Comments/Commands/Edit/EditCommentRequest.cs
@@ -0,0 +1,46 @@
+﻿using ShareBook.Application.Common;
+using ShareBook.Application.Common.Contracts;
+using ShareBook.Domain.Common;
+using ShareBook.Domain.Models;
+using ShareBook.Shared.Abstractions.Requests;
+
+namespace ShareBook.Application.Comments.Commands.Edit;
+public class EditCommentRequest : EntityCommand<Guid>, IRequest<Result>
+{
+	public string Content { get; set; }
+
+	public class EditCommentHandler : IRequestHandler<EditCommentRequest, Result>
+	{
+		private readonly ICurrentUser _currentUser;
+		private readonly IDomainRepository<Comment, Guid> _comments;
+
+		public EditCommentHandler(
+			ICurrentUser currentUser,
+			IDomainRepository<Comment, Guid> comments)
+		{
+			_currentUser = currentUser;
+			_comments = comments;
+		}
+
+		public async Task<Result> Handle(EditCommentRequest request, CancellationToken cancellationToken = default)
+		{
+			var comment = await _comments.FindAsync(request.Id, cancellationToken);
+			if (comment is null)
+			{
+				return Result.Failure(new []{ "Requested comment does not exist. "});
+			}
+			
+			if (comment.Author.Id != _currentUser.UserId)
+			{
+				return Result.Failure(new []{ "You do not own the requested comment."});
+			}
+			
+			comment.EditContent(request.Content);
+			var result = await _comments.SaveAsync(comment, cancellationToken);
+			
+			return result is not null
+				? Result.Success
+				: Result.Failure(new[] {"Could not save the requested comment."});
+		}
+	}
+}
