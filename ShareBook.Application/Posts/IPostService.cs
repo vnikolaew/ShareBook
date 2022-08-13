491491b5a763bec187da02b19e0cbaf7552f5eb3
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:38:54 +0300

    Added definition for a Post Service that interacts with Post entities ...

---
+++ ShareBook.Application/Posts/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using ShareBook.Application.Posts.Commands.Create;
+using ShareBook.Domain.Models.Post;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Application.Posts;
+
+public interface IPostService
+{
+	Task<Post?> Find(Guid id);
+	Task<Post?> Details(Guid id);
+	Task<Post?> Create(User user, CreatePostRequest request);
+	Task<IEnumerable<Post>> GetAllByUserId(UserId userId);
+	Task<bool> Delete(Guid postId);
+	Task<Post> Update(Post newPost);
+}
