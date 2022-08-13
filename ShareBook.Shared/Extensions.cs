346451d0297e85f070bc297cb0504f80c8d46887
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:32:18 +0300

    Added a simple dispatcher implementation that uses Reflection to find the appropriate handler and pass the application request ...

---
+++ ShareBook.Shared/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShareBook.Shared.Abstractions.Requests;
+using System.Reflection;
+
+namespace ShareBook.Shared;
+
+public static class Extensions
+{
+	public static IServiceCollection AddRequests(
+		this IServiceCollection services,
+		params Assembly[] assemblies)
+		=> services
+		   .AddSingleton<IRequestDispatcher, InMemoryRequestDispatcher>()
+		   .Scan(s => s.FromAssemblies(assemblies)
+		               .AddClasses(c =>
+			               c.AssignableToAny(
+				                typeof (IRequestHandler<>),
+				                typeof (IRequestHandler<,>))
+			                .Where(t => !t.IsAbstract && !t.IsInterface))
+		               .AsImplementedInterfaces()
+		               .WithScopedLifetime());
+}
---
+++ ShareBook.Shared/InMemoryRequestDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using ShareBook.Shared.Abstractions.Requests;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShareBook.Shared;
+
+public class InMemoryRequestDispatcher : IRequestDispatcher
+{
+  private readonly IServiceScopeFactory _scopeFactory;
+
+  public InMemoryRequestDispatcher(IServiceScopeFactory scopeFactory)
+   => _scopeFactory = scopeFactory;
+
+  public async Task Dispatch<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+      where TRequest : IRequest
+  {
+    await using var scope = _scopeFactory.CreateAsyncScope();
+    
+    var handler = scope.ServiceProvider.GetRequiredService<IRequestHandler<TRequest>>();
+    
+    await handler.Handle(request, cancellationToken);
+  }
+
+  public async Task<TResult> Dispatch<TRequest, TResult>(
+    TRequest request,
+    CancellationToken cancellationToken = default)
+      where TRequest : IRequest<TResult>
+  {
+    await using var scope = _scopeFactory.CreateAsyncScope();
+    var handler = scope.ServiceProvider
+                       .GetRequiredService<IRequestHandler<TRequest, TResult>>();
+    
+    return await handler.Handle(request, cancellationToken);
+  }
+}
