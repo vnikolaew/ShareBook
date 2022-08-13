ba6a4282308faa5b4855414f0cf0e5c62395d767
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:28:52 +0300

    Added a definition for a Post domain repository ..

---
+++ ShareBook.Domain/Models/Post/Repositories/IPostRepository.cs
@@ -0,0 +1,10 @@
+﻿using ShareBook.Domain.Common;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Domain.Models.Post.Repositories;
+
+public interface IPostRepository : IDomainRepository<Post, Guid>
+{
+  Task<IEnumerable<Post>> GetByUserId(UserId userId);
+  Task<Post?> FindWithDetails(Guid id, CancellationToken cancellationToken = default);
+}
