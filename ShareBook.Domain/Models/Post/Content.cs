aa0f6697a68e6e0fdd167c553fccec2fff61bf08
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:24:24 +0300

    Added a Content value object encapsulating all Content-related Entities ...

---
+++ ShareBook.Domain/Models/Post/Content.cs
@@ -0,0 +1,23 @@
+﻿using ShareBook.Domain.Common;
+using ShareBook.Domain.Models.Post.Exceptions;
+using static ShareBook.Domain.Models.ModelConstants.Post;
+
+namespace ShareBook.Domain.Models.Post;
+public class Content : ValueObject
+{
+  public string Value { get; }
+
+  public Content(string value)
+  {
+    Guard.ForStringLength<InvalidContentException>(value, MinContentLength, MaxContentLength);
+    Value = value;
+  }
+
+  public static implicit operator string(Content content)
+    => content.Value;
+
+  public static implicit operator Content(string content)
+    => new(content);
+
+  public override string ToString() => Value;
+}
