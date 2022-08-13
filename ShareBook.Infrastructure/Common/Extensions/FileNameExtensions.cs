900309b424e3cc89281b42902d7b6649fd4b091e
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:58:54 +0300

    Added a base Api Controller for abstracting the request dispatching process ...

---
+++ ShareBook.Infrastructure/Common/Extensions/FileNameExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+namespace ShareBook.Infrastructure.Common.Extensions;
+
+public static class FileNameExtensions
+{
+	private static readonly FileExtensionContentTypeProvider ContentTypeProvider
+		= new();
+	private const string DefaultContentType = "application/octet-stream";
+
+	public static string GetContentType(this string fileName)
+	{
+		return !ContentTypeProvider.TryGetContentType(fileName, out var contentType)
+			? DefaultContentType
+			: contentType;
+	}
+
+	public static string GetFileExtension(this string fileName)
+		=> fileName[(fileName.LastIndexOf('.') + 1)..];
+}
---
+++ ShareBook.Web/Common/ApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShareBook.Application.Common;
+using ShareBook.Shared.Abstractions.Requests;
+using ShareBook.Web.Extensions;
+
+namespace ShareBook.Web.Common;
+
+[ApiController]
+[Route("[controller]")]
+public class ApiController : ControllerBase
+{
+	protected const char PathSeparator = '/';
+	protected const string GuidId = "{id:guid}";
+
+	public IRequestDispatcher RequestDispatcher
+		=> HttpContext
+		   .RequestServices
+		   .GetRequiredService<IRequestDispatcher>();
+
+	protected Task<IActionResult> SendAsync<TRequest>(TRequest request)
+		where TRequest : IRequest<Result>
+		=> RequestDispatcher
+		   .Dispatch<TRequest, Result>(request)
+		   .ToActionResult();
+
+	protected Task<IActionResult> SendAsync<TRequest, TResult>(TRequest request)
+		where TRequest : IRequest<Result<TResult>>
+		=> RequestDispatcher
+		   .Dispatch<TRequest, Result<TResult>>(request)
+		   .ToActionResult();	
+}
