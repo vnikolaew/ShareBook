3717bdaaa9c19a184f895b5e00d5ef7353c995a4
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:19:56 +0300

    Adding a Profile Domain Entity containing more User specific information ...

---
+++ ShareBook.Domain/Models/Profile/Profile.cs
@@ -0,0 +1,25 @@
+﻿using ShareBook.Domain.Common;
+
+namespace ShareBook.Domain.Models.Profile;
+public class Profile : AuditableEntity<Guid>
+{
+  public FullName FullName { get; private set; }
+
+  public Bio Bio { get; private set; }
+
+  public Media.Media Photo { get; private set; }
+
+  public Gender Gender { get; private set; }
+
+  public Profile(FullName fullName, Bio bio, Gender gender)
+  {
+    FullName = fullName;
+    Bio = bio;
+    Gender = gender;
+  }
+
+  public Profile() { }
+
+  internal void UpdateBio(Bio bio) => Bio = bio;
+  internal void UpdateGender(Gender gender) => Gender = gender;
+}
