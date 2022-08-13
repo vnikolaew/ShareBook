100da9da3c563b9b622da90b4229829af0689211
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:24:53 +0300

    Added a factory component for creating Post entities ...

---
+++ ShareBook.Domain/Models/Post/Factories/IPostFactory.cs
@@ -0,0 +1,13 @@
+﻿using ShareBook.Domain.Common;
+
+namespace ShareBook.Domain.Models.Post.Factories;
+public interface IPostFactory : IFactory<Post>
+{
+  IPostFactory WithAuthor(User.User user);
+
+  IPostFactory WithContent(Content content);
+
+  IPostFactory WithContent(string content);
+
+  IPostFactory WithPhoto(Media.Media photo);
+}
---
+++ ShareBook.Domain/Models/Post/Factories/PostFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace ShareBook.Domain.Models.Post.Factories;
+
+public class PostFactory : IPostFactory
+{
+  private User.User _author;
+  private Content _content;
+  private Models.Media.Media _media;
+
+  public IPostFactory WithAuthor(User.User author)
+  {
+    _author = author;
+    return this;
+  }
+
+  public IPostFactory WithContent(Content content)
+  {
+    _content = content;
+    return this;
+  }
+
+  public IPostFactory WithContent(string content)
+    => WithContent(new Content(content));
+
+  public IPostFactory WithPhoto(Models.Media.Media photo)
+  {
+    _media = photo;
+    return this;
+  }
+
+  public Post Build()
+    => new(_author, _content, _media);
+}
