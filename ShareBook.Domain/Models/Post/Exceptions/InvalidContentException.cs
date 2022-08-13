6d9bd907e82ac647f5aa486b634d90e751559aba
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:25:18 +0300

    Added a Content-related validation exception ...

---
+++ ShareBook.Domain/Models/Post/Exceptions/InvalidContentException.cs
@@ -0,0 +1,8 @@
+﻿using ShareBook.Domain.Common;
+
+namespace ShareBook.Domain.Models.Post.Exceptions;
+public class InvalidContentException : ShareBookException
+{
+  public InvalidContentException(string message) => Message = message;
+  public InvalidContentException() { }
+}
