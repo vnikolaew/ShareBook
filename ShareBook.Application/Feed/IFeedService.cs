f45ca23c1353f0250e3b37a921103be89fe37634
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:56:17 +0300

    Added a User's Feed feature ...

---
+++ ShareBook.Application/Feed/IFeedService.cs
@@ -0,0 +1,9 @@
+﻿using ShareBook.Domain.Models.Post;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Application.Feed;
+
+public interface IFeedService
+{
+	Task<IEnumerable<Post>> Generate(UserId id, CancellationToken cancellationToken = default);
+}
---
+++ ShareBook.Application/Feed/Queries/Mine/GetMyFeedRequest.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ShareBook.Application.Common;
+using ShareBook.Application.Common.Contracts;
+using ShareBook.Shared.Abstractions.Requests;
+
+namespace ShareBook.Application.Feed.Queries.Mine;
+public class GetMyFeedRequest : IRequest<Result<FeedOutputModel>>
+{
+	public class GetMyFeedHandler : IRequestHandler<GetMyFeedRequest, Result<FeedOutputModel>>
+	{
+		private readonly IFeedService _feed;
+		private readonly ICurrentUser _currentUser;
+		private readonly IMapper _mapper;
+
+		public GetMyFeedHandler(
+			IFeedService feed,
+			ICurrentUser currentUser,
+			IMapper mapper)
+		{
+			_feed = feed;
+			_currentUser = currentUser;
+			_mapper = mapper;
+		}
+
+		public async Task<Result<FeedOutputModel>> Handle(GetMyFeedRequest request, CancellationToken cancellationToken = default)
+		{
+			var feed = await _feed.Generate(_currentUser.UserId, cancellationToken);
+			return new FeedOutputModel
+			{
+				Count = feed.Count(),
+				Posts = _mapper.Map<IEnumerable<FeedPostOutputModel>>(feed)
+			};
+		}
+	}
+
+}
