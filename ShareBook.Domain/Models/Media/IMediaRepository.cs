fcc50af4670d5b2e195b248e54f0ce290a417903
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:28:15 +0300

    Added a Media domain entity holding information about Media asses such as photos, videos, etc ...

---
+++ ShareBook.Domain/Models/Media/IMediaRepository.cs
@@ -0,0 +1,10 @@
+﻿using ShareBook.Domain.Common;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Domain.Models.Media;
+
+public interface IMediaRepository : IDomainRepository<Media, Guid>
+{
+  Task<Media> SaveProfileMediaAsync(UserId userId, Media media);
+  Task<Media> GetByUserIdAsync(UserId userId);
+}
---
+++ ShareBook.Domain/Models/Media/Media.cs
@@ -0,0 +1,18 @@
+﻿using ShareBook.Domain.Common;
+
+namespace ShareBook.Domain.Models.Media;
+public class Media : AuditableEntity<Guid>
+{
+  public string MediaName { get; private set; }
+  public string MediaType { get; private set; }
+  public string AbsoluteUrl { get; private set; }
+
+  public Media(string mediaName, string mediaType, string absoluteUrl)
+  {
+    MediaName = mediaName;
+    MediaType = mediaType;
+    AbsoluteUrl = absoluteUrl;
+  }
+
+  public Media() { }
+}
