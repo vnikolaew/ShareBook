b0973b9fef2dd76887f2d8e441ceba1ec0504a47
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:20:44 +0300

    Adding Profile-related domain exceptions ...

---
+++ ShareBook.Domain/Models/Profile/Exceptions/BioLengthOutOfRangeException.cs
@@ -0,0 +1,11 @@
+﻿using ShareBook.Domain.Common;
+
+namespace ShareBook.Domain.Models.Profile.Exceptions;
+
+public class BioLengthOutOfRangeException : ShareBookException
+{
+  public BioLengthOutOfRangeException(string message)
+    => Message = message;
+
+  public BioLengthOutOfRangeException() { }
+}
---
+++ ShareBook.Domain/Models/Profile/Exceptions/InvalidFullNameException.cs
@@ -0,0 +1,11 @@
+﻿using ShareBook.Domain.Common;
+
+namespace ShareBook.Domain.Models.Profile.Exceptions;
+
+public class InvalidFullNameException : ShareBookException
+{
+  public InvalidFullNameException(string message)
+    => Message = message;
+
+  public InvalidFullNameException() { }
+}
