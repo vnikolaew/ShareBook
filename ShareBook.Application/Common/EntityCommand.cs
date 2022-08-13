7d50ecf098c1167e499abe92fb3f34ca8dc7d5c9
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:43:01 +0300

    Added some Entity command extensions ...

---
+++ ShareBook.Application/Common/EntityCommand.cs
@@ -0,0 +1,8 @@
+﻿using ShareBook.Shared.Abstractions.Requests;
+
+namespace ShareBook.Application.Common;
+
+public abstract class EntityCommand<TId> : IRequest
+{
+	public TId Id { get; set; }
+}
---
+++ ShareBook.Application/Common/EntityCommandExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace ShareBook.Application.Common;
+
+public static class EntityCommandExtensions
+{
+	public static TCommand WithId<TCommand, TId>(this TCommand command, TId id)
+		where TCommand : EntityCommand<TId>
+	{
+		command.Id = id;
+		return command;
+	}
+}
