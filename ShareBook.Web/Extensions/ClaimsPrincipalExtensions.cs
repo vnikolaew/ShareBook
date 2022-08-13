5dd086134dc488217b1b39caab35e78a31236c78
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 19:02:41 +0300

    Added some more useful extension classes for working with the ClaimsPrincipal as well as translating a IFormFile object to an application request ...

---
+++ ShareBook.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace ShareBook.Web.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+	public static string? GetId(this ClaimsPrincipal claimsPrincipal)
+		=> claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+
+	public static string? GetEmail(this ClaimsPrincipal claimsPrincipal)
+		=> claimsPrincipal.FindFirstValue(ClaimTypes.Email);
+
+	public static string? GetUserName(this ClaimsPrincipal claimsPrincipal)
+		=> claimsPrincipal.FindFirstValue(ClaimTypes.Name);	
+}
---
+++ ShareBook.Web/Extensions/RequestExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ShareBook.Application.Posts.Commands.Create;
+using ShareBook.Application.Profile.Commands.EditPhoto;
+
+namespace ShareBook.Web.Extensions;
+
+public static class RequestExtensions
+{
+	public static EditProfilePhotoRequest ToRequest(this IFormFile file)
+		=> new()
+		{
+			Content = file.OpenReadStream(),
+			ContentType = file.ContentType,
+			FileName = file.FileName
+		};
+
+	public static PostMediaModel ToPostMediaModel(this IFormFile file)
+		=> new()
+		{
+			Content = file.OpenReadStream(),
+			ContentType = file.ContentType,
+			MediaName = file.FileName
+		};
+}	
