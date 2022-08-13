fc5df576774c479c4aaa883d9e1aa3e7613d677a
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:36:08 +0300

    Added an application contract for an Identity Service ...

---
+++ ShareBook.Application/Identity/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using ShareBook.Application.Identity.Commands;
+using ShareBook.Application.Identity.Commands.SignUp;
+using ShareBook.Application.Identity.Queries.Authenticate;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Application.Identity;
+public interface IIdentityService
+{
+  Task<AuthenticationResult> SignUpAsync(SignUpRequestModel requestModel);
+  Task<AuthenticationResult> Authenticate(AuthRequestModel authRequestModel);
+
+  Task<User?> FindById(UserId userId,
+    CancellationToken cancellationToken = default);
+}
