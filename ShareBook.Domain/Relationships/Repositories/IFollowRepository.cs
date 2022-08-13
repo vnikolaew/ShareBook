434cab90d86c8d5ebf718973a762e27dec01f71b
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:26:59 +0300

    Added specific repository interfaces for delaing with Follows and Likes relationships ...

---
+++ ShareBook.Domain/Relationships/Repositories/IFollowRepository.cs
@@ -0,0 +1,11 @@
+﻿using ShareBook.Domain.Common;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Domain.Relationships.Repositories;
+
+public interface IFollowRepository : IRelationshipRepository<Follows, User, User, UserId, UserId>
+{
+  Task<IEnumerable<Follows>> GetFollowsAsync(UserId userId, CancellationToken cancellationToken = default);
+
+  Task<IEnumerable<Follows>> GetFollowersAsync(UserId userId, CancellationToken cancellationToken = default);
+}
---
+++ ShareBook.Domain/Relationships/Repositories/ILikeRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShareBook.Domain.Common;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Domain.Relationships.Repositories;
+
+public interface ILikeRepository : IRelationshipRepository<Liked, User, Models.Post.Post, UserId, Guid>
+{
+  Task<IEnumerable<Liked>> GetAllByPostId(Guid id, CancellationToken cancellationToken = default);
+}
