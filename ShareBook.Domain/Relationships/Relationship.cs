960cab9bff8afc9567500dd8e26d2680bd02dd59
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:25:56 +0300

    Added a base definition for a Relationship Entity (note that is is meant to be directional) ...

---
+++ ShareBook.Domain/Relationships/Relationship.cs
@@ -0,0 +1,11 @@
+﻿using ShareBook.Domain.Common;
+
+namespace ShareBook.Domain.Relationships;
+public abstract class Relationship<TFrom, TTo> : Entity<Guid>
+  where TFrom : IEntity
+  where TTo : IEntity
+{
+  public TFrom EntityOne { get; protected set; }
+
+  public TTo EntityTwo { get; protected set; }
+}
