d34a5a26d151f03f68e7c5d0abe06184e81913c1
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 19:05:17 +0300

    Adding the Startup and Program classes that serve as an entry point to the whole application ...

---
+++ ShareBook.Web/Program.cs
@@ -0,0 +1,14 @@
+﻿using ShareBook.Web.Extensions;
+
+namespace ShareBook.Web;
+public class Program
+{
+	public static void Main(string[] args)
+		=> CreateDefaultBuilder(args)
+		   .UseStartup<Startup>()
+		   .Run();
+	private static WebApplicationBuilder CreateDefaultBuilder(string[] args)
+		=> WebApplication
+				.CreateBuilder(args)
+				.ConfigureKestrelHttps(5064, 7064);
+}
---
+++ ShareBook.Web/Startup.cs
@@ -0,0 +1,43 @@
+﻿using ShareBook.Application;
+using ShareBook.Infrastructure;
+using ShareBook.Web.Extensions;
+
+namespace ShareBook.Web;
+public class Startup
+{
+	public Startup(IConfiguration configuration)
+		=> Configuration = configuration;
+
+	private IConfiguration Configuration { get; set; }
+
+	public void ConfigureServices(IServiceCollection services)
+		=> services
+				.AddApplication()
+				.AddInfrastructure(Configuration)
+				.AddWebComponents();
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if (!env.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Home/Error")
+			   .UseHsts();
+		}
+		else
+		{
+			app.UseSwagger()
+			   .UseSwaggerUI();
+		}
+		
+		app
+			.UseHttpsRedirection()
+			.UseRouting()
+			.UseAuthentication()
+			.UseAuthorization()
+			.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapFallbackRoute();
+			});
+	}	
+}
