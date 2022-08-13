a5151bcf980606f58be7ceb54ef4ef24364cf4bf
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:47:48 +0300

    Added definitions for Likes and Media specific services ...

---
+++ ShareBook.Application/Likes/ILikesService.cs
@@ -0,0 +1,15 @@
+﻿using ShareBook.Domain.Models.User;
+using ShareBook.Domain.Relationships;
+
+namespace ShareBook.Application.Likes;
+
+public interface ILikesService
+{
+	Task<bool> Like(UserId likerId, Guid likedPostId,
+		DateTime createdOn, CancellationToken cancellationToken = default);
+
+	Task<bool> Unlike(UserId likerId, Guid likedPostId,
+		CancellationToken cancellationToken = default);
+
+	Task<IEnumerable<Liked>> GetAllByPostId(Guid id, CancellationToken cancellationToken = default);
+}
---
+++ ShareBook.Application/Profile/IMediaService.cs
@@ -0,0 +1,9 @@
+﻿using ShareBook.Application.Profile.Commands.EditPhoto;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Application.Profile;
+
+public interface IMediaService
+{
+	Task<bool> SaveProfileMediaAsync(UserId userId, EditProfilePhotoRequest request);
+}
