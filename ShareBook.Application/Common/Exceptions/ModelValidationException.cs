4916db63a897a7a053137fd7833fe47a5295505e
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:57:28 +0300

    Added a helper Exception class for translating any validation errors caught by Fluent Validation ...

---
+++ ShareBook.Application/Common/Exceptions/ModelValidationException.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+
+namespace ShareBook.Application.Common.Exceptions;
+
+public class ModelValidationException : Exception
+{
+	public ModelValidationException()
+		: base("One or more validation exceptions have occured")
+		=> Errors = new Dictionary<string, string[]>();
+
+	public IDictionary<string, string[]> Errors { get; set; }
+
+	public ModelValidationException(IEnumerable<ValidationFailure> failures)
+		: this()
+	{
+		foreach (var source in failures.GroupBy(fg => fg.PropertyName, fg => fg.ErrorMessage))
+		{
+			Errors.Add(source.Key, source.ToArray());
+		}
+	}
+}
