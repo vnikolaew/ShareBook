44595f8613e727ddf3881592a8d12aa6e967a885
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 18:21:15 +0300

    Adding a profile repository definition for retrieving user profiles ...

---
+++ ShareBook.Domain/Models/Profile/Repositories/IProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShareBook.Domain.Common;
+using ShareBook.Domain.Models.User;
+
+namespace ShareBook.Domain.Models.Profile.Repositories;
+
+public interface IProfileRepository : IDomainRepository<Profile, Guid>
+{
+  Task<Profile?> GetByUserId(UserId id, CancellationToken cancellationToken = default);
+}
