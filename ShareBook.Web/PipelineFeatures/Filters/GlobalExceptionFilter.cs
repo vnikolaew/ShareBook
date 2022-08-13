030e883aac192956e3ae85fd18b676159146d848
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-13 19:04:05 +0300

    Added exception filters to the ASP.NET application pipeline for catching any thrown exception anywhere in the call stack ...

---
+++ ShareBook.Web/PipelineFeatures/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ShareBook.Web.PipelineFeatures.Filters;
+public class GlobalExceptionFilter : IAsyncExceptionFilter
+{
+	public Task OnExceptionAsync(ExceptionContext context)
+	{
+		context.HttpContext.Response.ContentType = MediaTypeNames.Application.Json;
+		context.HttpContext.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+		var exception = context.Exception;
+		context.Result = new BadRequestObjectResult(new 
+		{
+			exception.Message,
+			StatusCode = (int) HttpStatusCode.BadRequest,
+		});
+		
+		context.ExceptionHandled = true;
+		return Task.CompletedTask;
+	}
+}
---
+++ ShareBook.Web/PipelineFeatures/Filters/ValidationExceptionFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mime;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ShareBook.Application.Common.Exceptions;
+
+namespace ShareBook.Web.PipelineFeatures.Filters;
+
+public class ValidationExceptionFilter : IAsyncExceptionFilter
+{
+	public Task OnExceptionAsync(ExceptionContext context)
+	{
+		if (context.Exception is not ModelValidationException exception)
+			return Task.CompletedTask;
+
+		context.HttpContext.Response.ContentType = MediaTypeNames.Application.Json;
+		context.HttpContext.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+
+		context.Result = new BadRequestObjectResult(new ErrorResponseModel
+		{
+			Message = exception.Message,
+			StatusCode = (int) HttpStatusCode.BadRequest,
+			Errors = exception.Errors
+		});
+		context.ExceptionHandled = true;
+		
+		return Task.CompletedTask;
+	}
+
+	public class ErrorResponseModel
+	{
+		public string Message { get; set; }
+		public int StatusCode { get; set; }
+		public IDictionary<string, string[]> Errors { get; set; }
+	}
+}
