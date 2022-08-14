4b8c3b78f6571a078fb85832395aa738cf5cc765
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:53:02 +0300

    Added a Feed Controller ...

---
+++ ShareBook.Web/Features/FeedController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShareBook.Application.Feed;
+using ShareBook.Application.Feed.Queries.Mine;
+using ShareBook.Web.Common;
+
+namespace ShareBook.Web.Features;
+
+[Authorize]
+public class FeedController : ApiController
+{
+	[HttpGet]
+	public Task<IActionResult> Generate()
+		=> SendAsync<GetMyFeedRequest, FeedOutputModel>(new GetMyFeedRequest());	
+}
