dc736cdd2536a57bf365edc4c5d2c273dcecf697
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 21:19:54 +0300

    Added a Post API Client ...

---
+++ ShareBook.ClientSdk/Clients/Posts/Responses/PostOutputModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ShareBook.ClientSdk.Clients.Posts.Responses;
+
+public class PostsResponse
+{
+	public int Count { get; set; }
+	public IEnumerable<PostResponseModel> Posts { get; set; }
+}
+
+public class PostResponseModel
+{
+	public string Id { get; set; }
+	public DateTime CreatedOn { get; set; }
+	public DateTime LastModifiedOn { get; set; }
+	public string Content { get; set; }
+	public PostPhotoResponseModel Media { get; set; }
+}
+
+public class PostPhotoResponseModel
+{
+	public string Id { get; set; }
+	public string PhotoUrl { get; set; }
+}
