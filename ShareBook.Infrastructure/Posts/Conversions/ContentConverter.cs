cb0d5922f4998ffde0eefaa7f862254c07c17e8b
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:34:43 +0300

    Added some more mappings and conversion definitions related to the Post entity ,,,

---
+++ ShareBook.Infrastructure/Posts/Conversions/ContentConverter.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using ShareBook.Domain.Models.Post;
+using ShareBook.Infrastructure.Identity.Conversions;
+
+namespace ShareBook.Infrastructure.Posts.Conversions;
+
+public class ContentConverter : ValueObjectConverter<Content>
+{
+	public override Content? ReadJson(JsonReader reader, Type objectType, Content? existingValue, bool hasExistingValue,
+		JsonSerializer serializer) => new(reader.Value as string);
+}
---
+++ ShareBook.Infrastructure/Posts/PostMapping.cs
@@ -0,0 +1,10 @@
+﻿using ShareBook.Domain.Models.Post;
+using ShareBook.Infrastructure.Common.Persistence.Mappings;
+
+namespace ShareBook.Infrastructure.Posts;
+
+public class PostMapping : INodeMapping<Post>
+{
+	public object Map(Post entity)
+		=> new { entity.Content, entity.CreatedOn ,entity.ModifiedOn};
+}
