a82f1b4602f66ea7a379bdfa72b8826837e699a7
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:47:43 +0300

    Added infrastructure service for storing and uploading media assets to Azure Blob Storage ....

---
+++ ShareBook.Infrastructure/Images/Services/IFileStorageUploadService.cs
@@ -0,0 +1,12 @@
+﻿namespace ShareBook.Infrastructure.Images.Services;
+
+public interface IFileStorageUploadService
+{
+	Task<bool> UploadFileAsync(string absoluteFilePath, string fileName);
+
+	Task<bool> UploadFileAsync(Stream data, string fileName);
+
+	string GetAbsoluteFileUrl(string fileName);
+
+	Task<bool> DeleteFileAsync(string blobName);	
+}
---
+++ ShareBook.Infrastructure/Images/Settings/AzureBlobStorageSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ShareBook.Infrastructure.Images.Settings;
+public class AzureBlobStorageSettings
+{
+	public string AccountName { get; set; }
+	public string AccountKey { get; set; }
+	public string EndpointSuffix { get; set; }	
+	
+	public string ConnectionString
+		=> $"DefaultEndpointsProtocol=https;{nameof(AccountName)}={AccountName};{nameof(AccountKey)}={AccountKey};{nameof(EndpointSuffix)}={EndpointSuffix}";
+}
