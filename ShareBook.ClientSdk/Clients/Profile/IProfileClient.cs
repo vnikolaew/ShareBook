9ec8cf2cd4344f7dc9881f4905dbc6dd21b9b8d0
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 21:20:13 +0300

    Added a Profile Client

---
+++ ShareBook.ClientSdk/Clients/Profile/IProfileClient.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+using ShareBook.ClientSdk.Clients.Profile.Requests;
+using ShareBook.ClientSdk.Clients.Profile.Responses;
+
+namespace ShareBook.ClientSdk.Clients.Profile;
+
+public interface IProfileClient
+{
+	[Put("/profile")]
+	Task<IApiResponse> Edit(EditProfileRequest request, [Authorize] string token);
+
+	[Put("/profile/photo")]
+	Task<IApiResponse> EditProfilePhoto([AliasAs("image")]StreamPart image, [Authorize] string token);
+
+	[Get("/profile")]
+	Task<ApiResponse<ProfileResponse>> Mine( [Authorize] string token);
+	
+	[Get("/profile/{id:guid}")]
+	Task<ApiResponse<ProfileResponse>> ByUser(Guid id, [Authorize] string token);
+}
