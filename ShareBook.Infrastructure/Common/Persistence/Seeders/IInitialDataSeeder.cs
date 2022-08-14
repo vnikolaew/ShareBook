963d240375f38788842ae007ee8cbfe45e4c1a8b
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:18:36 +0300

    Added a sample Social Media Seeder Service that can initially seed the graph database with sample data generated by the Faker library ...

---
+++ ShareBook.Infrastructure/Common/Persistence/Seeders/IInitialDataSeeder.cs
@@ -0,0 +1,6 @@
+﻿namespace ShareBook.Infrastructure.Common.Persistence.Seeders;
+
+public interface IInitialDataSeeder
+{
+	Task SeedAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken = default);	
+}
