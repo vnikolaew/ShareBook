e3663cc21f3abcc33df50b93dba66a13d9444a51
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:49:52 +0300

    Added an Authentication API Controller ...

---
+++ ShareBook.Web/Features/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShareBook.Application.Common.Contracts;
+using ShareBook.Application.Identity.Commands;
+using ShareBook.Application.Identity.Commands.SignUp;
+using ShareBook.Application.Identity.Queries.Authenticate;
+using ShareBook.Web.Common;
+using ShareBook.Web.Extensions;
+
+namespace ShareBook.Web.Features;
+public class AuthController : ApiController
+{
+	private const string LoginEndpoint = "login";
+	private const string MeEndpoint = "me";
+	
+	[HttpPost]
+	[AllowAnonymous]
+	[Route(nameof(SignUp))]
+	public Task<IActionResult> SignUp(SignUpRequest signUpRequest)
+		=> RequestDispatcher
+		   .Dispatch<SignUpRequest, AuthenticationResult>(signUpRequest)
+		   .ToActionResult();
+
+	[HttpPost]
+	[AllowAnonymous]
+	[Route(LoginEndpoint)]
+	public Task<IActionResult> Authenticate(AuthenticateRequest authRequest)
+		=> RequestDispatcher
+		   .Dispatch<AuthenticateRequest, AuthenticationResult>(authRequest)
+			.ToActionResult();
+
+	[Authorize]
+	[HttpGet(MeEndpoint)]
+	public IActionResult PersonalDetails([FromServices] ICurrentUser currentUser)
+		=> Ok(currentUser.Details);
+
+}
