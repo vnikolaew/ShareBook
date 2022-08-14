3c530c38d4d91026472f4cf83d8bba3e93f14881
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:20:35 +0300

    Added a helper Dictionary-like cache of accessors that can be accesses anywhere in the application ...

---
+++ ShareBook.Infrastructure/Common/Reflection/IAccessorProvider.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+
+namespace ShareBook.Infrastructure.Common.Reflection;
+
+public interface IAccessorProvider
+{
+	Accessor<TSource, TDest> Get<TSource, TDest>(Expression<Func<TSource, TDest>> selector);
+}
