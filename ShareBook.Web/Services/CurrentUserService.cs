7b676275e6a4daff3f620894ac82bb22b9c334d1
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:51:16 +0300

    Added a web service for accessing information / context about the current application user ...

---
+++ ShareBook.Web/Services/CurrentUserService.cs
@@ -0,0 +1,31 @@
+﻿using ShareBook.Application.Common.Contracts;
+using ShareBook.Web.Extensions;
+
+namespace ShareBook.Web.Services;
+
+public class CurrentUserService : ICurrentUser
+{
+	public CurrentUserService(IHttpContextAccessor accessor)
+	{
+		var claimsPrincipal
+			= accessor.HttpContext.User
+			  ?? throw new InvalidOperationException("The request does not have an authenticated user.");
+
+		UserId = claimsPrincipal.GetId();
+		UserName = claimsPrincipal.GetUserName();
+		Details = new PersonalDetailsModel
+		{
+			Id = UserId,
+			Name = UserName,
+			Claims = accessor.HttpContext.User.Claims.Select(c => new PersonalDetailsModel.Claim()
+			{
+				Type = c.Type,
+				Value = c.Value
+			})
+		};
+	}
+	
+	public string? UserId { get; }
+	public string? UserName { get; }
+	public IPersonalDetails? Details { get; }
+}
---
+++ ShareBook.Web/Services/PersonalDetailsModel.cs
@@ -0,0 +1,16 @@
+﻿using ShareBook.Application.Common.Contracts;
+
+namespace ShareBook.Web.Services;
+
+public class PersonalDetailsModel : IPersonalDetails
+{
+	public string Name { get; init; }
+	public string Id { get; init; }
+	public IEnumerable<Claim> Claims { get; set; }
+
+	public class Claim
+	{
+		public string Type { get; set; }
+		public string Value { get; set; }
+	}
+}
