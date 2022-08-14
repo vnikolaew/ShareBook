bfcbd2bbabb2caed11d488e2d93083c696d75a8c
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:40:38 +0300

    Added components for the Comments feature ...

---
+++ ShareBook.Infrastructure/Comments/CommentMapping.cs
@@ -0,0 +1,10 @@
+﻿using ShareBook.Domain.Models;
+using ShareBook.Infrastructure.Common.Persistence.Mappings;
+
+namespace ShareBook.Infrastructure.Comments;
+
+public class CommentMapping : INodeMapping<Comment>
+{
+	public object Map(Comment entity)
+		=> new {entity.Content};
+}
