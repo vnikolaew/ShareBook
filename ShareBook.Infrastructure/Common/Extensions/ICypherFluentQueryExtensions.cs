6ee283050b62b84410f76a74226d00e12b6b9062
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 20:15:12 +0300

    Added some useful query result extensions to help with database query building ...

---
+++ ShareBook.Infrastructure/Common/Extensions/ICypherFluentQueryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Neo4jClient.Cypher;
+
+namespace ShareBook.Infrastructure.Common.Extensions;
+
+public static class ICypherFluentQueryExtensions
+{
+	public static async Task<TResult> GetResult<TResult>(
+		this ICypherFluentQuery<TResult> query,
+		CancellationToken cancellationToken = default)
+		=> (await query.ResultsAsync.WaitAsync(cancellationToken)).SingleOrDefault<TResult>();
+
+	public static async Task<IEnumerable<TResult>> GetResults<TResult>(
+		this ICypherFluentQuery<TResult> query,
+		CancellationToken cancellationToken = default)
+		=> await query.ResultsAsync.WaitAsync(cancellationToken);
+}
