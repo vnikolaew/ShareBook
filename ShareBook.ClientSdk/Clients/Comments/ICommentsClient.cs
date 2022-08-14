3261a4bb5f9aafb47a8dfa7894222a91a9cc7f46
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 21:20:37 +0300

    Added a Comments Client ...

---
+++ ShareBook.ClientSdk/Clients/Comments/ICommentsClient.cs
@@ -0,0 +1,13 @@
+﻿using Refit;
+using ShareBook.ClientSdk.Clients.Comments.Requests;
+
+namespace ShareBook.ClientSdk.Clients.Comments;
+
+public interface ICommentsClient
+{
+	[Post("/comments/{id:guid}")]
+	Task<IApiResponse> Comment([Body] CreateCommentRequest request, Guid id, [Authorize] string token);
+
+	[Put("/comments/{id:guid}")]
+	public Task<IApiResponse> Edit([Body] EditCommentRequest request, Guid id, [Authorize] string token);
+}
