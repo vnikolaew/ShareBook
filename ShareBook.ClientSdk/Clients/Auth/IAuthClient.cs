922a198a4bd1b584b9ee493657b26d40622101a6
Victorio Nikolaev <vikinik01@abv.bg>
2022-08-14 21:19:22 +0300

    Added an Auth API Client along with the expected requests and responses ...

---
+++ ShareBook.ClientSdk/Clients/Auth/IAuthClient.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using ShareBook.ClientSdk.Clients.Requests;
+using ShareBook.ClientSdk.Clients.Responses;
+
+namespace ShareBook.ClientSdk.Clients;
+
+public interface IAuthClient
+{
+	[Post("/auth/login")]
+	Task<ApiResponse<AuthenticationResult>> Authenticate([Body] AuthenticateRequest request);
+	
+	[Post("/auth/signup")]
+	Task<ApiResponse<AuthenticationResult>> SignUp([Body] SignUpRequest request);
+
+	[Get("/auth/me")]
+	Task<ApiResponse<PersonalDetails>> PersonalDetails([Authorize] string token);
+}
